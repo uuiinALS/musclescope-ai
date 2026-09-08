b4cc7c4440af4a89d18c56226225c5ccb073f0ef
uuiinALS <1070476198@qq.com>
2026-09-08 13:13:47 +0800

    feat: add week one hands-on exercises

---
+++ exercises/week01_tasks.py
@@ -0,0 +1,26 @@
+"""第1周练习。先自己完成TODO，再查看src中的参考实现。"""
+
+
+def add(a: float, b: float) -> float:
+    # TODO(day1): 返回两数之和
+    raise NotImplementedError
+
+
+def maximum(numbers: list[float]) -> float:
+    # TODO(day1): 不使用内置max；自行决定空列表如何处理
+    raise NotImplementedError
+
+
+def distance(x1: float, y1: float, x2: float, y2: float) -> float:
+    # TODO(day1): 使用勾股定理计算两点距离
+    raise NotImplementedError
+
+
+def parse_point(data: dict[str, object]) -> tuple[float, float]:
+    # TODO(day4): 读取x/y，检查类型；非法输入抛ValueError
+    raise NotImplementedError
+
+
+def calculate_angle(first: tuple[float, float], vertex: tuple[float, float], third: tuple[float, float]) -> float:
+    # TODO(day5): 用点积公式实现，不调用src里的joint_angle
+    raise NotImplementedError
